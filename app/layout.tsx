664ef8b53e305c3ff3dfeba40d8518e68baf34d2
Veronica Rojas <ar_veronica@yahoo.com>
2023-11-13 19:53:13 +1000

    template for layout file and import globals.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import "./globals.css";
 export const metadata = {
   title: 'Flexibble',
   description: 'Showcase and discover remarkable developer projects',
@@ -10,7 +11,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      
+      <body>
+      
+        Navbar
+        <main>
+          {children}
+        </main>
+        Footer
+        
+      </body>
+      
     </html>
   )
 }
