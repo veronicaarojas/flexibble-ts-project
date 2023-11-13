a60c7bd0eec737841269483a3aed556e516de976
Veronica Rojas <ar_veronica@yahoo.com>
2023-11-13 20:51:33 +1000

    complete header and footer

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,14 @@
+import { NavLinks } from '@/constants'
 import Image from 'next/image'
 import Link from 'next/link'
+import AuthProviders from './AuthProviders'
 
 
 function Navbar() {
+  const session = {}
   return (
     <nav className='flexBetween navbar'>
-      <div className='flex-1 flex-start gap-10'>
+      <div className='flex-1 flexStart gap-10'>
         <Link href="/">
         <Image
         src="/logo.svg"
@@ -14,8 +17,26 @@ function Navbar() {
         alt="logo"/>
         </Link>
         <ul className='xl:flex hidden text-small gap-7'>
-
+          {NavLinks.map((link) => (
+            <Link href={link.href} key={link.key}>
+              {link.text}
+            </Link>
+          ))}
         </ul>
+      </div>
+
+      <div className='flexCenter gap-4'>
+            {session ? (
+              <>
+              User Photo
+
+              <Link href="/create-project">
+              Share Work
+              </Link>
+              </>
+            ) : (
+              <AuthProviders />
+            )}
       </div>
     </nav>
   ) 
