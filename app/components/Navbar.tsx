392eeafe890cd566944f76e9d8b34a6d0a374847
Veronica Rojas <ar_veronica@yahoo.com>
2023-11-13 20:10:58 +1000

    create constants file

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,12 +6,16 @@ function Navbar() {
   return (
     <nav className='flexBetween navbar'>
       <div className='flex-1 flex-start gap-10'>
-        <Link href="/"></Link>
+        <Link href="/">
         <Image
         src="/logo.svg"
         width={115}
         height={43}
         alt="logo"/>
+        </Link>
+        <ul className='xl:flex hidden text-small gap-7'>
+
+        </ul>
       </div>
     </nav>
   ) 
