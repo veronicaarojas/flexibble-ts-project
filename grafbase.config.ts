77ae530fbe545ae84247e96cfd4b3cd5af57a95f
Veronica Rojas <ar_veronica@yahoo.com>
2023-11-14 18:36:42 +1000

    set up grafbase and create user and project models

---
+++ grafbase.config.ts
@@ -0,0 +1,27 @@
+import { g, config } from '@grafbase/sdk'
+
+
+const User = g.model('User', {
+  name: g.string().length({min: 2, max: 35}),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string(),
+  githubUrl: g.url().optional(),
+  linkedInUrl: g.url().optional(),
+  projects: g.relation(() => Project).list().optional(),
+})
+
+const Project = g.model('Project', {
+  title: g.string().length({min: 3}),
+  description: g.string().optional(),
+  image: g.url(),
+  liveSiteUrl: g.url().optional(),
+  githubUrl: g.url().optional(),
+  category: g.string().search(),
+  createdBy: g.relation(() => User),
+})
+
+export default config({
+  schema: g
+
+})
