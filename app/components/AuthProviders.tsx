0e463e3d1fe2ac34c08a62de90e4df83db21f6ae
Veronica Rojas <ar_veronica@yahoo.com>
2023-11-16 15:44:39 +1000

    create Provider type for auth provider

--- app/components/AuthProviders.tsx
+++ app/components/AuthProviders.tsx
@@ -1,6 +1,20 @@
-import React from 'react'
+'use client'
+
+import { getProviders, signIn } from 'next-auth/react'
+import { useState, useEffect } from "react"
+
+type Providers ={
+  id: string;
+  name: string;
+  type: string;
+  signInUrl: string;
+  callbackUrl: string;
+  signInUrlParams:  Record<string, string> | null;
+
+}
 
 function AuthProviders() {
+  const [ providers, setProviders ] = useState<Providers | null >(null);
   return (
     <div>AuthProviders</div>
   )
