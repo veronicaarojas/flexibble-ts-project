2f44feda0a07374370e4c2c9a5fe3cd140e74564
Veronica Rojas <ar_veronica@yahoo.com>
2023-12-04 15:21:14 +1000

    export auth providers

--- lib/session.ts
+++ lib/session.ts
@@ -4,3 +4,12 @@ import { AdapterUser } from 'next-auth/adapters'
 import GoogleProvider from 'next-auth/providers/google'
 import jsonwebtoken from 'jsonwebtoken'
 import { JWT } from 'next-auth/jwt'
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: '',
+      clientSecret: ''
+    })
+  ]
+}
